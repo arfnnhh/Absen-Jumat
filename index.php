868dab7a74c34cdc909070a57be52fde5c6f890a
komangkysatridarma <komangkysatridarma@smkwikrama.sch.id>
2023-09-13 11:46:33 +0700

    login page

--- index.php
+++ index.php
@@ -13,6 +13,116 @@
 </head>
 <body>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<style>
+*{
+    font-family:'Popins', sans-serif;
+    text-decoration:none;
+    color:#fff;
+}
+body{
+    background-image:url("wave-haikei (1).svg");
+    background-size:cover;
+    background-repeat:repeat;
+}
+.box{
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    min-height:90vh;
+}
+.container{
+    width:350px;
+    display:flex;
+    flex-direction:column;
+    padding:0 15px 0 15px;
+}
+header{
+    color:#fff;
+    font-size:30px;
+    display:flex;
+    justify-content:center;
+    padding:10px 0 10px 0;
+}
+.input-field{
+    display:flex;
+    flex-direction:column;
+    margin:10px 0;
+}
+.input-field1{
+    display:flex;
+    flex-direction:column;
+    margin-left:30px;
+}
+.input{
+    height:45px;
+    width:100%;
+    border:none;
+    outline:none;
+    padding:0 0 0 45px;
+    border-radius:30px;
+    color:#fff;
+    background: rgba(255,255,255,0.1);
+}
+::-webkit-input-placeholder{
+    color:#fff;
+}
+.submit{
+    border:none;
+    border-radius:30px;
+    font-size:15px;
+    height:45px;
+    outline:none;
+    width:89%;
+    background:rgba(255,255,255,0.7);
+    cursor:pointer;
+    transition:.3s;
+}
+.submit:hover{
+    box-shadow:1px 5px 7px 1px rgba(0,0,0,0.2);
+}
+p{
+    color:red;
+    font-style:italic;
+}
+a{
+    margin-top:10px;
+}
+</style>
+<body>
+    <div class="box">
+        <div class="container">
+            <div class="top-header">
+                <header>Login</header>
+            </div>
+
+            <?php if(isset($error)) : ?>
+                <p>Username/Password Salah!</p>
+            <?php endif; ?>
+
+            <form action="" method="post">
+                <div class="input-field">
+                    <input type="text" name="username" class="input" placeholder="Username" required>
+                    <i class="bx bx-user"></i>
+                </div>
+                <div class="input-field">
+                    <input type="password" name="password" class="input" placeholder="Password" required>
+                    <i class="bx bx-lock-alt"></i>
+                </div>
+                <div class="input-field1">
+                <button type="submit" name="login" class="submit">Login!</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.js"></script>
 </body>
 </html>
